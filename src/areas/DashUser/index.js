8a11c7fd0822060f2ba2100bf050669db866bf1a
Wilson Ramos <hupcontato@gmail.com>
2023-11-27 17:34:25 -0300

    melhorias

--- src/areas/DashUser/index.js
+++ src/areas/DashUser/index.js
@@ -16,9 +16,12 @@ export default function DashUser({ indice, Payment }) {
 
     const [file, setFile] = useState(null)
 
-    const { setClient, client, getApi, searchApi, setStatusConnection } = useContext(AppContext)
-    const [route, setRoute] = useState('Loja')
+    const { client, getApi, searchApi, setStatusConnection } = useContext(AppContext)
+    const [route] = useState('Loja')
     const [paramsID, setParamsID] = useState(client?.userID)
+    const [numberMonth, setNumberMonth] = useState(0)
+
+    const [statusPayment, setStatusPayment] = useState(null)
 
     useEffect(() => {
         setParamsID(client?.userID)
@@ -35,29 +38,19 @@ export default function DashUser({ indice, Payment }) {
         }
 
         var today = moment();
-        var futureDate = today.add(4, 'months');
+        var futureDate = today.add(numberMonth, 'months');
 
-        // if (!!registerPayment) {
-        //     await getApi(searchApi).put(`payment?paymentID=${registerPayment?.id}`, {
-        //         value: valuePayment,
-        //         expiration: futureDate.format('DD/MM/YYYY')
-        //     }, { headers })
-        //         .then(() => console.log("atualizado"))
-        //         .finally(() => setClient(client))
-
-        // } else {
-
-            await getApi(searchApi).post(`payment?userID=${client?.userID}`, {
-                value: valuePayment,
-                expiration: futureDate.format('DD/MM/YYYY')
-            }, { headers })
-                .then((response) => console.log(response))
-                .catch((error) => console.log(error))
+        await getApi(searchApi).post(`payment?userID=${client?.userID}`, {
+            value: valuePayment,
+            expiration: futureDate.format('DD/MM/YYYY')
+        }, { headers })
+            .then((response) => console.log(response))
+            .catch((error) => console.log(error))
         // }
     }
 
     async function HandlePayment() {
-        await getApi(searchApi).get(`payment?userID=${client?.userID}`)
+        await getApi(searchApi).get(`payments?userID=${client?.userID}`)
             .then((response) => setRegisterPayment(response.data))
             .catch((error) => {
                 if (error.message === 'Network Error') {
@@ -110,6 +103,17 @@ export default function DashUser({ indice, Payment }) {
             .catch(() => alert("Erro ao enviar banner"))
     }
 
+    async function PutStatusPayment(e, payment) {
+        let authGuia = localStorage.getItem('@authGuia')
+        let credentials = await JSON.parse(authGuia)
+        const headers = {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${credentials.token}`
+        }
+        await getApi(searchApi).put(`payment?paymentID=${payment}`, { status: e.target.value }, { headers })
+        .then(() => window.location.reload(true))
+    }
+
     const HandleFile = (e) => {
         if (e.target.files[0]) {
             Resizer.imageFileResizer(
@@ -138,8 +142,41 @@ export default function DashUser({ indice, Payment }) {
         }}>
 
 
-            <div style={{ height: 40, marginBottom: 2, alignItems: 'center', justifyContent: 'center', display: 'flex', gap: 12 }}>
+            <div>
                 <div style={{ color: '#aaa', fontSize: 14 }}>{client?.name}</div>
+
+                <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+                    <input placeholder='0' style={{ border: 'none', padding: '0px 12px', height: 35, width: '100%', backgroundColor: '#282c34', color: '#aaa' }} onChange={(e) => setNumberMonth(e.target.value)} />
+                    <input placeholder='R$ 0,00' style={{ border: 'none', padding: '0px 12px', height: 35, width: '100%', backgroundColor: '#282c34', color: '#aaa' }} onChange={(e) => setValuePayment(e.target.value)} />
+                    <button disabled={!client || !valuePayment} onClick={() => Payment()} style={{ height: 35, border: 'none', backgroundColor: '#2E7D32', color: '#aaa', padding: '0 18px' }}>Pagar</button>
+                </div>
+            </div>
+
+            <div>
+                {registerPayment?.filter((payment) =>
+                    payment.userID === paramsID
+                ).map((item) => {
+                    return (
+                        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+
+                            <div style={{ display: 'flex', flex: 1, color: '#aaa', fontSize: 12, height: 30, alignItems: 'center' }}>{moment(item.createdAt).format('DD/MM')}</div>
+                            <div style={{ display: 'flex', flex: 1, color: '#aaa', fontSize: 12, height: 30, alignItems: 'center' }}>{item.value}</div>
+                            <div style={{ display: 'flex', flex: 1, color: '#aaa', fontSize: 12, height: 30, alignItems: 'center' }}>{item.status}</div>
+                            <select defaultValue={'Select'} onChange={(e) => PutStatusPayment(e, item.id)}  >
+                                <option value={'Select'}>Selecione</option>
+                                <option value={'Processando'}>Processando</option>
+                                <option value={'Aprovado'}>Aprovado</option>
+                                <option value={'Vencido'}>Vencido</option>
+                            </select>
+                        </div>
+                    )
+                })}
+            </div>
+
+            {/* {console.log(client)}
+
+
+            <div style={{ height: 40, marginBottom: 2, alignItems: 'center', justifyContent: 'center', display: 'flex', gap: 12 }}>
                 <div style={{ width: 6, height: 6, borderRadius: 10, backgroundColor: isOnline ? '#76FF03' : "#777" }} />
             </div>
 
@@ -161,7 +198,7 @@ export default function DashUser({ indice, Payment }) {
             <span style={{ padding: '0px 12px', height: 35, width: '100%', backgroundColor: '#282c34', fontSize: 14, color: '#aaa', display: "flex", alignItems: 'center' }}>Profissão: {client?.profession?.name}</span>
 
             <span onClick={() => registerPayment?.createdAt && alert("Ultimo registro de pagamento: " + moment(registerPayment?.createdAt).format('DD/MM/YYYY'))} style={{ display: 'flex', alignItems: 'center', padding: '0px 12px', height: 35, width: '100%', backgroundColor: '#282c34', color: '#aaa', fontSize: 14 }}>Venc.: {registerPayment?.datePayment} - {diffDias >= 0 ? diffDias + " dias" : "0"} </span>
-
+ */}
 
 
         </div>
