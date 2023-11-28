0c46a060f9687c8ae62fabbd78c0a58c56ee7aef
Wilson Ramos <hupcontato@gmail.com>
2023-11-28 12:31:53 -0300

    melhorias

--- src/App.js
+++ src/App.js
@@ -48,7 +48,7 @@ export default function App() {
           <ListUsers setDash={setDash} dash={dash} />
           <ListCategories />
           <ListProfessions />
-          {dash && <DashUser />}
+          <DashUser />
 
         </div>
       </div>
--- src/areas/DashUser/index.js
+++ src/areas/DashUser/index.js
@@ -3,7 +3,7 @@ import { useState, useContext, useEffect } from "react"
 import Resizer from "react-image-file-resizer";
 import { AppContext } from "../../contexts";
 
-export default function DashUser({ indice, Payment }) {
+export default function DashUser({ Payment }) {
 
     const [datePayment, setDatePayment] = useState(null)
     const [valuePayment, setValuePayment] = useState(0)
@@ -111,7 +111,7 @@ export default function DashUser({ indice, Payment }) {
             'Authorization': `Bearer ${credentials.token}`
         }
         await getApi(searchApi).put(`payment?paymentID=${payment}`, { status: e.target.value }, { headers })
-        .then(() => window.location.reload(true))
+        .then(() => HandlePayment())
     }
 
     const HandleFile = (e) => {
@@ -152,18 +152,17 @@ export default function DashUser({ indice, Payment }) {
                 </div>
             </div>
 
-            <div>
+            <div style={{marginTop:6}}>
                 {registerPayment?.filter((payment) =>
                     payment.userID === paramsID
                 ).map((item) => {
                     return (
                         <div style={{ display: 'flex', justifyContent: 'space-between' }}>
 
-                            <div style={{ display: 'flex', flex: 1, color: '#aaa', fontSize: 12, height: 30, alignItems: 'center' }}>{moment(item.createdAt).format('DD/MM')}</div>
+                            <div style={{ display: 'flex', flex: 1, color: '#aaa', fontSize: 12, height: 30, alignItems: 'center' }}>{moment(item.createdAt).format('DD/MM/YY')}</div>
                             <div style={{ display: 'flex', flex: 1, color: '#aaa', fontSize: 12, height: 30, alignItems: 'center' }}>{item.value}</div>
-                            <div style={{ display: 'flex', flex: 1, color: '#aaa', fontSize: 12, height: 30, alignItems: 'center' }}>{item.status}</div>
-                            <select defaultValue={'Select'} onChange={(e) => PutStatusPayment(e, item.id)}  >
-                                <option value={'Select'}>Selecione</option>
+                            <select defaultValue={item.status} onChange={(e) => PutStatusPayment(e, item.id)} style={{background:'#282c34', color:"#aaa", padding:'0 12px'}} >
+                                <option>Selecione</option>
                                 <option value={'Processando'}>Processando</option>
                                 <option value={'Aprovado'}>Aprovado</option>
                                 <option value={'Vencido'}>Vencido</option>
@@ -173,34 +172,7 @@ export default function DashUser({ indice, Payment }) {
                 })}
             </div>
 
-            {/* {console.log(client)}
-
-
-            <div style={{ height: 40, marginBottom: 2, alignItems: 'center', justifyContent: 'center', display: 'flex', gap: 12 }}>
-                <div style={{ width: 6, height: 6, borderRadius: 10, backgroundColor: isOnline ? '#76FF03' : "#777" }} />
-            </div>
-
-            <div style={{ border: '1px solid #aaa' }}>
-
-                <input style={{ border: 'none', padding: '0px 12px', height: 35, width: '100%', backgroundColor: '#282c34', color: '#aaa' }} type='date' onChange={(e) => setDatePayment(moment(e.target.value).format("DD/MM/YYYY"))} />
-                <input placeholder='R$ 0,00' style={{ border: 'none', padding: '0px 12px', height: 35, width: '100%', backgroundColor: '#282c34', color: '#aaa' }} onChange={(e) => setValuePayment(e.target.value)} />
-                <button disabled={!client || !valuePayment} onClick={() => Payment()} style={{ height: 35, border: 'none', backgroundColor: '#2E7D32', color: '#aaa', width: '100%' }}>Pagar</button>
-            </div>
-
-            <div style={{ border: '.5px solid #aaa', marginTop: 6 }}>
-
-                <label style={{ height: 35, border: 'none', color: '#aaa', width: '100%', fontSize: 14, display: "flex", alignItems: 'center', justifyContent: 'center' }} for="arquivo">{!!file ? "Banner Capturado" : "Selecionar Banner"}</label>
-
-                <input style={{ width: '100%', display: 'none' }} type="file" id="arquivo" onChange={(e) => HandleFile(e)} />
-                {!!file ? <label onClick={() => SendBanner()} style={{ height: 35, border: 'none', backgroundColor: '#2E7D32', fontSize: 14, color: '#aaa', width: '100%', display: "flex", alignItems: 'center', justifyContent: 'center' }} for="arquivo">Enviar Banner</label> : null}
-            </div>
-
-            <span style={{ padding: '0px 12px', height: 35, width: '100%', backgroundColor: '#282c34', fontSize: 14, color: '#aaa', display: "flex", alignItems: 'center' }}>Profissão: {client?.profession?.name}</span>
-
-            <span onClick={() => registerPayment?.createdAt && alert("Ultimo registro de pagamento: " + moment(registerPayment?.createdAt).format('DD/MM/YYYY'))} style={{ display: 'flex', alignItems: 'center', padding: '0px 12px', height: 35, width: '100%', backgroundColor: '#282c34', color: '#aaa', fontSize: 14 }}>Venc.: {registerPayment?.datePayment} - {diffDias >= 0 ? diffDias + " dias" : "0"} </span>
- */}
-
-
+       
         </div>
 
     )
